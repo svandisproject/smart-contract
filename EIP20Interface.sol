63a8d5d599f1f5a434c785c38b1addefb34c583c
victorwiebe <vw@akropolis.io>
2018-05-29 17:16:19 -0400

    Changes to interface and Svandis

--- EIP20Interface.sol
+++ EIP20Interface.sol
@@ -2,6 +2,24 @@
 // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
 pragma solidity ^0.4.19;
 
+library SafeMath {
+    function add(uint a, uint b) internal pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+    function sub(uint a, uint b) internal pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+    function mul(uint a, uint b) internal pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+    function div(uint a, uint b) internal pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
 
 contract EIP20Interface {
     uint256 public totalSupply;
