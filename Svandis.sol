39f62cc9b9767e4dcd1f656b3862480ac71ce948
victorwiebe <vw@akropolis.io>
2018-05-29 17:22:18 -0400

    Finish adding safemath and contributor allowed

--- Svandis.sol
+++ Svandis.sol
@@ -2,9 +2,27 @@ pragma solidity ^0.4.21;
 
 import "./EIP20Interface.sol";
 
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a + b;
+        require(c >= a);
+    }
+    function sub(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        require(b <= a);
+        c = a - b;
+    }
+    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+    function div(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
 
 contract Svandis is EIP20Interface {
-
+    using SafeMath for uint256;
     uint256 constant private MAX_UINT256 = 2**256 - 1;
     uint256 constant public totalSupply = 400000000000000000000000000;
     uint8 constant public decimals = 18;
@@ -19,7 +37,7 @@ contract Svandis is EIP20Interface {
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balances[msg.sender] >= _value);
         balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[to].add(_value);
+        balances[_to] = balances[_to].add(_value);
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
